360134591e4bd00505b97df5eceb573614b3390a
Samms206 <sh2069936@gmail.com>
2024-04-25 18:06:10 +0700

    firts commit

---
+++ app/src/main/java/com/example/uts_pember/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.uts_pember;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_buka);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(500);
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+
+        // Memulai animasi pada tombol
+        button.startAnimation(anim);
+    }
+
+    public void gotoWedding(View view) {
+    }
+}
